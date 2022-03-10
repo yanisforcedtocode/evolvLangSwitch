36a566da26049ef59f0a2cb8c24ad59ba112cbbc
Yan Yan Yan <hello@mymoobi.com>
2022-03-10 22:52:47 +0800

    Restructuring script - class based with functional components

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
+'usestrict'
 console.log(document.title)
 
+
+
 const pickerOpts = {
-    types: [
-      {
-        description: 'config filter',
-        accept: {
-          'config/*': ['.config']
-        }
-      },
-    ],
-    excludeAcceptAllOption: true,
-    multiple: false
+
   };
 
 const newLang = 'abc'
 
-async function getFile() {
-  // open file picker, destructure the one element returned array
-  const fileHandle = await window.showOpenFilePicker(pickerOpts);
-  // run code with our fileHandle
-  return fileHandle
-}
 
-const openDirBtn = document.querySelector("#openDirPicker01")
+
+
+
+
 openDirBtn.addEventListener("click", async(e)=>{
     console.log("open dir picker")
     const handles = await getFile()
@@ -35,11 +26,13 @@ openDirBtn.addEventListener("click", async(e)=>{
         //console.log(contents)
         const parser = new DOMParser();
         let xmlDoc = parser.parseFromString(contents,"text/xml");
+        // 
         let culture = xmlDoc.querySelector('[name="Culture"]')
         let value = culture.querySelector('value')
         // value = newLang
         value.textContent = newLang
         console.log(value.textContent)
+        //
         const xmlSerializer = new XMLSerializer();
         const str = xmlSerializer.serializeToString(xmlDoc);
         console.log(str)
@@ -53,3 +46,79 @@ openDirBtn.addEventListener("click", async(e)=>{
     }
 }
 )
+
+class LangChanger {
+  constructor(params) {
+    this.langs = params.langs
+    this.pickerOpts = params.pickerOpts
+    this.openDirBtn = document.querySelector("#openDirPicker01")
+    // this.zhCNBtn = document.querySelector("#zhCNBtn")
+    // this.zhTWBtn = document.querySelector("#zhTWBtn")
+    // this.enGBBtn = document.querySelector("#enGBBtn")
+    this.iniState = {
+      isPickedFile:false,
+      isSelectedLang:false,
+      isSavedFile:false,
+    }
+  }
+  // Handlers
+  async getFile() {
+    // open file picker, destructure the one element returned array
+    const fileHandles = await window.showOpenFilePicker(this.pickerOpts);
+    // run code with our fileHandle
+    return fileHandles
+  }
+  async returnTxtContent(handles){
+    let contents = []
+    for (let handle of handles){
+      console.log(handle)
+      const file = await handle.getFile();
+      const contents = await file.text();
+      contents.push(contents)
+    }
+    return contents[0]
+  }
+  returnXMLDoc(content){
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(content,"text/xml");
+    return xmlDoc
+  }
+  changeLangValue(xmlDoc, newLang){
+    let culture = xmlDoc.querySelector('[name="Culture"]')
+    let value = culture.querySelector('value')
+    value.textContent = newLang
+    console.log(value.textContent)
+  }
+  serializeXML(xmlDoc){
+    const xmlSerializer = new XMLSerializer();
+    const str = xmlSerializer.serializeToString(xmlDoc);
+    console.log(str)
+    return str
+  }
+  async saveFile(str, handle){
+    const writable = await handle.createWritable();
+    await writable.write(str);
+    await writable.close();
+  }
+
+}
+
+const langChanger = new LangChanger({
+  langs: [
+    { name: "English", value: "en-GB" },
+    { name: "繁體中文", value: "zh-TW" },
+    { name: "簡體中文", value: "zh-TW" },
+  ],
+  pickerOpts: {
+    types: [
+      {
+        description: "config filter",
+        accept: {
+          "config/*": [".config"],
+        },
+      },
+    ],
+    excludeAcceptAllOption: true,
+    multiple: false,
+  },
+});
