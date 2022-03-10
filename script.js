54c28b897287c9b451c8b8d4146206d1b9cae04f
Yan <hello@mymoobi.com>
2022-03-10 21:56:00 +0800

    First working proto - File system access API - Read a *.config file - Parse in xml - Serialize edited xml - Save the same file

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const pickerOpts = {
     multiple: false
   };
 
+const newLang = 'abc'
 
 async function getFile() {
   // open file picker, destructure the one element returned array
@@ -27,7 +28,28 @@ openDirBtn.addEventListener("click", async(e)=>{
     const handles = await getFile()
   // run code for dirHandle
     for (let handle of handles){
-        console.log(handle.name)
+        console.log(handle)
+        const file = await handle.getFile();
+        const contents = await file.text();
+
+        //console.log(contents)
+        const parser = new DOMParser();
+        let xmlDoc = parser.parseFromString(contents,"text/xml");
+        let culture = xmlDoc.querySelector('[name="Culture"]')
+        let value = culture.querySelector('value')
+        // value = newLang
+        value.textContent = newLang
+        console.log(value.textContent)
+        const xmlSerializer = new XMLSerializer();
+        const str = xmlSerializer.serializeToString(xmlDoc);
+        console.log(str)
+
+        // Create a FileSystemWritableFileStream to write to.
+        const writable = await handle.createWritable();
+        // Write the contents of the file to the stream.
+        await writable.write(str);
+        // Close the file and write the contents to disk.
+        await writable.close();
     }
 }
 )
